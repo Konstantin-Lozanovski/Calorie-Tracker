f51e4f37d3c55b6b1cb3fe0cc98a3ff1a346a6c4
Konstantin Lozanovski <lozanovskikoko@gmail.com>
2025-12-07 21:05:20 +0100

    Implemented the food controllers

--- backend/controllers/food.controller.js
+++ backend/controllers/food.controller.js
@@ -1,13 +1,44 @@
 import { pool } from "../src/db.js"
+import {BadRequestError} from "../errors/index.js";
 
 export const searchFoods = async (req, res) => {
-  const { search } = req.query
-  // TODO: SELECT * FROM foods WHERE name ILIKE ...
-  const result = await pool.query("SELECT * FROM foods WHERE name ILIKE $1", [`%${search}%`])
-  res.json(result.rows)
+    const { search } = req.query
+
+    if(!search || search.trim().length === 0) throw new BadRequestError("Please provide a search term")
+
+    const result = await pool.query(
+        `SELECT * FROM foods 
+         WHERE name ILIKE $1
+         OR COALESCE(brand, '') ILIKE $1
+         LIMIT 20`,
+        [`%${search}%`]
+    )
+
+    res.status(200).json(result.rows)
 }
 
 export const createFood = async (req, res) => {
-  // TODO: Insert new food into foods table
-  res.send("Create new Food")
+    let {name, brand,calories,protein,carbs,fat,serving_unit} = req.body;
+
+    name = name?.trim();
+    brand = brand?.trim() || null;
+    serving_unit = serving_unit?.trim();
+
+    if (!name || !serving_unit || calories == null || protein == null || carbs == null || fat == null) {
+        throw new BadRequestError("Please provide all required fields");
+    }
+
+    if (isNaN(calories) || isNaN(protein) || isNaN(carbs) || isNaN(fat)) {
+        throw new BadRequestError("Calories, protein, carbs, and fat must be numbers");
+    }
+
+    const result = await pool.query(
+        `INSERT INTO foods (name, brand, calories, protein, carbs, fat, serving_unit)
+        VALUES ($1, $2, $3, $4, $5, $6, $7)
+        RETURNING *`,
+        [name,brand,calories,protein,carbs,fat,serving_unit]
+    )
+
+    res.status(201).json(result.rows[0])
+
 }
