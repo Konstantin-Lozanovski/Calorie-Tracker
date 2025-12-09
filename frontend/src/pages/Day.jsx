05b7c36c1f38a37c0b65f931850aa59180973a16
Konstantin Lozanovski <lozanovskikoko@gmail.com>
2025-12-09 19:58:34 +0100

    Implemented fetchDay api call and Day page
    Implemented the fetchDay api call in api.js and dispalyed the data

--- frontend/src/pages/Day.jsx
+++ frontend/src/pages/Day.jsx
@@ -1,14 +1,56 @@
 import { useParams } from "react-router-dom";
+import {useState, useEffect} from "react";
+import { fetchDay} from "../services/api.js";
 
 const Day = ({ user }) => {
     const { date } = useParams(); // date is the string from URL
+    const [dayLog, setDayLog] = useState({});
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        const getDay = async () => {
+            try {
+                setLoading(true);
+                const data = await fetchDay(date);
+                setDayLog(data);
+            }catch(error){
+                setError(error.msg || "Failed to fetch daily log");
+            }finally {
+                setLoading(false);
+            }
+        }
+
+        getDay()
+    },[date, user.id])
+
+    if(loading) return <div>Loading...</div>;
+    if(error) return <div>{error}</div>;
+
+    console.log(dayLog);
 
     return (
         <div>
-            <h1>Logs for {date}</h1>
-            {/* fetch the daily log using /api/days/:date */}
+            <h1>Daily Log: {dayLog.date}</h1>
+            {dayLog.meals.map(meal => (
+                <div key={meal.id}>
+                    <h2>{meal.name}</h2>
+                    {meal.entries.length ? (
+                        <ul>
+                            {meal.entries.map(entry => (
+                                <li key={entry.id}>
+                                    {entry.quantity} {entry.food.unit} {entry.food.name} - {entry.food.calories} cal
+                                </li>
+                            ))}
+                        </ul>
+                    ) : (
+                        <p>No entries yet</p>
+                    )}
+                </div>
+            ))}
         </div>
-    );
+    )
+
 };
 
 export default Day;
--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,1 +1,10 @@
 import axiosInstance from './axiosInstance';
+
+export const fetchDay = async (date) => {
+    try {
+        const response = await axiosInstance.get(`/api/days/${date}`)
+        return response.data
+    } catch (error) {
+        throw error.response?.msg || error.message || "Failed fetching employees"
+    }
+}
