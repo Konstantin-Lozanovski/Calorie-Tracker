94cf6ab68b89453f638f0f27289885cf81e35256
Konstantin Lozanovski <lozanovskikoko@gmail.com>
2025-12-09 18:33:34 +0100

    Implemented ProtectedRoute component
    Protected Route component redirects to login if no user is present and it reusable for different pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,8 @@ import { Routes, Route } from "react-router-dom"
 import Header from "./components/Header"
 import Signup from "./pages/Signup"
 import Login from "./pages/Login"
+import Dashboard from "./pages/Dashboard"
+import ProtectedRoute from "./components/ProtectedRoute"
 import { useEffect, useState } from "react"
 import { getUserFromLocalStorage } from "./services/auth"
 
@@ -10,7 +12,7 @@ function App() {
     const [loading, setLoading] = useState(true)
 
     useEffect(() => {
-        const storedUser = getUserFromLocalStorage()
+        const storedUser = getUserFromLocalStorage() || null
         if (storedUser) {
             setUser(storedUser)
         }
@@ -26,7 +28,14 @@ function App() {
             <Header user={user} setUser={setUser} />
             <main className='main-content'>
                 <Routes>
-                    <Route path='/' element={<h1>Home</h1>} />
+                    <Route
+                        path="/"
+                        element={
+                            <ProtectedRoute user={user} >
+                                <Dashboard user={user} setUser={setUser} />
+                            </ProtectedRoute>
+                        }
+                    />
                     <Route path='/signup' element={<Signup user={user} setUser={setUser} />} />
                     <Route path='/login' element={<Login user={user} setUser={setUser} />} />
                 </Routes>
---
+++ frontend/src/pages/Dashboard.jsx
@@ -0,0 +1,12 @@
+
+const Dashboard = ({user, setUser}) => {
+
+    return (
+        <>
+            <div>Dashboard</div>
+            <h1>{user.username}</h1>
+        </>
+    )
+}
+
+export default Dashboard
