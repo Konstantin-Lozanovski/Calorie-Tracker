8058f0284c2e7d808a9595e3a5f7ce59456dd8cb
Konstantin Lozanovski <lozanovskikoko@gmail.com>
2025-12-10 00:22:17 +0100

    Implemented the api calls for searchingFood and fetchingFood details by id

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -8,3 +8,21 @@ export const fetchDay = async (date) => {
     throw error.response?.msg || error.message || "Failed fetching employees"
   }
 }
+
+export const searchFoods = async (query) => {
+  try{
+    const response = await axiosInstance.get(`/api/foods?search=${query}`);
+    return response.data
+  }catch(error){
+    throw error.response?.msg || error.message || "Failed fetching foods"
+  }
+}
+
+export const fetchFood = async (foodId) => {
+  try{
+    const response = await axiosInstance.get(`/api/foods/${foodId}`);
+    return response.data
+  }catch(error){
+    throw error.response?.msg || error.message || "Failed fetching food"
+  }
+}
