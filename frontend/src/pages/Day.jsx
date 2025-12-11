072be2bbc5166d8a8e0499648356a5eb1adacf7f
Konstantin Lozanovski <lozanovskikoko@gmail.com>
2025-12-12 00:31:46 +0100

    Css Styling and refactoring
    Css styling and refactoring also removed the searching ability from this page cuz it shoudl be its own page

--- frontend/src/pages/Day.jsx
+++ frontend/src/pages/Day.jsx
@@ -1,7 +1,7 @@
 import {useParams} from "react-router-dom";
 import {useNavigate} from "react-router-dom";
 import {useState, useEffect} from "react";
-import { useMemo } from "react";
+import {useMemo} from "react";
 import {fetchDay, searchFoods, addEntry} from "../services/api.js";
 import "../css/Day.css";
 
@@ -11,13 +11,7 @@ const Day = ({user}) => {
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
 
-  const [activeMealId, setActiveMealId] = useState(null); // which meal’s panel is open
-  const [tab, setTab] = useState("search"); // "search" | "myFoods"
-  const [searchQuery, setSearchQuery] = useState("");
-  const [searchResults, setSearchResults] = useState([]);
-
   const navigate = useNavigate();
-
 
   const totals = useMemo(() => {
     let calories = 0;
@@ -25,7 +19,7 @@ const Day = ({user}) => {
     let carbs = 0;
     let fat = 0;
 
-    if (!dayLog.meals) return { calories, protein, carbs, fat };
+    if (!dayLog.meals) return {calories, protein, carbs, fat};
 
     dayLog.meals.forEach(meal => {
       meal.entries.forEach(entry => {
@@ -45,8 +39,6 @@ const Day = ({user}) => {
     };
   }, [dayLog]);
 
-
-
   useEffect(() => {
     const getDay = async () => {
       try {
@@ -63,120 +55,66 @@ const Day = ({user}) => {
     getDay()
   }, [date, user.id])
 
-  useEffect(() => {
-    if (searchQuery.length === 0) {
-      setSearchResults([]);
-      return;
-    }
-
-    const delayDebounce = setTimeout(async () => {
-      try {
-        const results = await searchFoods(searchQuery);
-        setSearchResults(results);
-      } catch (error) {
-        setError(error.msg || "Failed to fetch daily log");
-      }
-    }, 300); // debounce 300ms
-
-    return () => clearTimeout(delayDebounce); // cleanup
-  }, [searchQuery]);
-
-  const handleViewFood = (meal, food) => {
-    navigate(`/day/${date}/meal/${meal.id}/food/${food.id}`);
-  }
-
-  const handleAddFood = async (mealId, foodId) => {
-    try {
-      await addEntry(mealId, foodId, 100);
-
-      // 1. Refresh day log
-      const updatedDay = await fetchDay(date);
-      setDayLog(updatedDay);
-
-      // 2. Close the add-food panel
-      setActiveMealId(null);
-
-      // 3. Optionally reset search
-      setSearchQuery("");
-      setSearchResults([]);
-      setTab("search");
-    } catch (error) {
-      setError(error.msg || "Failed to add food");
-    }
-  };
-
-
   if (loading) return <div>Loading...</div>;
   if (error) return <div>{error}</div>;
 
   console.log(dayLog);
 
   return (
-    <div>
+    <div className="container">
       <h1>Daily Log: {dayLog.date}</h1>
       <h3>Totals</h3>
       <h1>GOAL:{user.calorie_goal}</h1>
       <p>Calories: {totals.calories} kcal</p>
-      <p>Protein: {totals.protein} g</p>
-      <p>Carbs: {totals.carbs} g</p>
-      <p>Fat: {totals.fat} g</p>
-      {dayLog.meals.map(meal => (
-        <div key={meal.id} className="meal">
-          <h2>{meal.name}</h2>
+      <div className="macros-container">
+        <p>Protein: {totals.protein} / {user.protein_goal_pct} g</p>
+        <p>Carbs: {totals.carbs} / {user.carbs_goal_pct} g</p>
+        <p>Fat: {totals.fat} / {user.fat_goal_pct} g</p>
+      </div>
 
-          {meal.entries.length ? (
-            <ul>
-              {meal.entries.map(entry => (
-                <li key={entry.id}>
-                  {entry.quantity} {entry.food.serving_unit}s {entry.food.name} - {(entry.quantity * entry.food.calories / 100).toFixed(1)} cal
-                </li>
-              ))}
-            </ul>
-          ) : <p>No entries yet</p>}
+      <h2>Meals</h2>
 
-          <button onClick={() => setActiveMealId(activeMealId === meal.id ? null : meal.id)}>
-            Add Food
-          </button>
+      <div className="meals-container">
 
-          {activeMealId === meal.id && (
-            <div className="add-food-panel">
-              {/* Tabs */}
-              <div className="tabs">
-                <button onClick={() => setTab("search")} className={tab === "search" ? "active" : ""}>Search</button>
-                <button onClick={() => setTab("myFoods")} className={tab === "myFoods" ? "active" : ""}>MyFoods</button>
+        {dayLog.meals.map(meal => {
+          const mealTotal = meal.entries.reduce((sum, entry) => {
+            return sum + (entry.quantity * entry.food.calories) / 100;
+          }, 0);
+
+          return (
+            <div key={meal.id} className="meal">
+              <div className="meal-header">
+                <h2>{meal.name} </h2>
+                <span className="entry-calories">{mealTotal.toFixed(0)} cal</span>
               </div>
 
-              {/* Tab content */}
-              {tab === "search" && (
-                <div className="search-tab">
-                  <input
-                    type="text"
-                    placeholder="Search foods..."
-                    value={searchQuery}
-                    onChange={e => setSearchQuery(e.target.value)}
-                  />
-                  <ul>
-                    {searchResults.map(food => (
-                      <li key={food.id}>
-                        {food.name} - {food.calories} cal
-                        <button onClick={() => handleAddFood(meal.id, food.id)}>Add</button>
-                        <button onClick={() => handleViewFood(meal, food)}>View</button>
-                      </li>
-                    ))}
-                  </ul>
-                </div>
-              )}
+              {meal.entries.length ? (
 
-              {tab === "myFoods" && (
-                <div>
-                  {/* Render user’s saved foods here */}
+                <div className="entries">
+                  {meal.entries.map(entry => (
+                    <div key={entry.id} className="entry-row">
+                      <span className="entry-name">{entry.food.name}</span>
+                      <span
+                        className="entry-details">{entry.food.brand} — {entry.quantity}{entry.food.serving_unit}</span>
+                      <span className="entry-calories">
+                      {(entry.quantity * entry.food.calories / 100).toFixed(1)} cal
+                    </span>
+                    </div>
+
+                  ))}
                 </div>
-              )}
+
+
+              ) : <p>No entries yet</p>}
+
+              <button onClick={() => navigate(`/day/${date}/meal/${meal.id}/food/${food.id}`)}>
+                Add Food
+              </button>
             </div>
-          )}
-        </div>
-      ))}
+          )
+        })}
 
+      </div>
     </div>
   )
 
