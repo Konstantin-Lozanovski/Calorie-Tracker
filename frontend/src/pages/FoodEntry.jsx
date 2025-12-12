a3ecab784f2693dee9a0a9266e7cb1103aea2b12
Konstantin Lozanovski <lozanovskikoko@gmail.com>
2025-12-12 02:20:34 +0100

    Update FoodEntry.jsx
    Changed FoodEntry.jsx to implement changing an entry and also adding an entry

--- frontend/src/pages/FoodEntry.jsx
+++ frontend/src/pages/FoodEntry.jsx
@@ -1,40 +1,57 @@
-import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import {useEffect, useState} from "react";
+import {useParams} from "react-router-dom";
 import {useNavigate} from 'react-router-dom';
-import {fetchFood, addEntry} from "../services/api.js";
+import {fetchFood, addEntry, fetchEntry, updateEntry} from "../services/api.js";
 
 export default function FoodEntry() {
-  const { date, mealId, foodId } = useParams();
+  const {date, mealId, foodId, entryId} = useParams();
+  const isEditing = Boolean(entryId);
+  const isAddingFromSearch = Boolean(foodId && !entryId);
 
   const [food, setFood] = useState(null);
+  const [entry, setEntry] = useState(null);
   const [quantity, setQuantity] = useState("100");
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState("");
+
 
   const navigate = useNavigate();
 
   // Fetch the specific food entry
   useEffect(() => {
-    const getFood = async () => {
-      try {
+
+    const load = async () => {
+      try{
         setLoading(true);
-        const data = await fetchFood(foodId);
-        setFood(data);
-      } catch (error) {
-        setError(error.msg || "Failed to fetch food");
+        if(isEditing) {
+          const entryData = await fetchEntry(entryId);
+          setEntry(entryData);
+          setQuantity(entryData.quantity.toString());
+          const foodData = await fetchFood(entryData.food_id)
+          setFood(foodData);
+        }else if(isAddingFromSearch){
+          const foodData = await fetchFood(foodId);
+          setFood(foodData);
+        }
+      }catch (error) {
+        setError(error.msg || "Failed to fetch entry");
       } finally {
         setLoading(false);
       }
-    };
+    }
 
-    getFood();
-  }, [date, mealId, foodId]);
+   load()
+  }, [date, mealId, foodId, entryId, isEditing, isAddingFromSearch]);
 
   const handleAdd = async () => {
-    try{
-      const data = await addEntry(mealId, foodId, quantity);
+    try {
+      if(isEditing){
+        const data = await updateEntry(entry.id, Number(quantity))
+      }else if(isAddingFromSearch){
+        const data = await addEntry(mealId, foodId, Number(quantity));
+      }
       navigate(`/day/${date}`)
-    }catch(error) {
+    } catch (error) {
       setError(error.msg || "Failed to add entry");
     }
   };
@@ -64,7 +81,9 @@ export default function FoodEntry() {
         />
       </label>
 
-      <button onClick={handleAdd}>Add</button>
+      <button onClick={handleAdd} disabled={loading}>{isEditing ? "Update" : "Add"}</button>
+
+
     </div>
   );
 }
