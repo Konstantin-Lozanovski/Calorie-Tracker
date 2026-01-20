ed6f11f7de127d29a7d362a86dc6043541180587
Konstantin Lozanovski <lozanovskikoko@gmail.com>
2026-01-20 23:42:02 +0100

    Updated auth schemas with body object

--- backend/schemas/auth.schema.js
+++ backend/schemas/auth.schema.js
@@ -1,12 +1,16 @@
 import { z } from "zod";
 
 export const registerSchema = z.object({
-  username: z.string().min(1, "Username is required").trim(),
-  email: z.string().email("Please provide a valid email").trim().transform((val) => val.toLowerCase()),
-  password: z.string().min(6, "Password must be at least 6 characters long"),
+  body: z.object({
+    username: z.string().min(1, "Username is required").trim(),
+    email: z.string().email("Please provide a valid email").trim().transform((val) => val.toLowerCase()),
+    password: z.string().min(6, "Password must be at least 6 characters long"),
+  })
 });
 
 export const loginSchema = z.object({
-  email: z.string().email("Please provide a valid email").trim().transform((val) => val.toLowerCase()),
-  password: z.string().min(1, "Password is required."),
+  body: z.object({
+    email: z.string().email("Please provide a valid email").trim().transform((val) => val.toLowerCase()),
+    password: z.string().min(1, "Password is required."),
+  })
 })
